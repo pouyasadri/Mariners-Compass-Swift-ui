9f475a96428a3cd08b678b44eb2ebb1b43c6dfe6
pouyasadri <pouyapaghandeh78@gmail.com>
2024-01-20 15:06:50 +0100

    Initial Commit

---
+++ CompossApp/CompossAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompossAppApp.swift
+//  CompossApp
+//
+//  Created by Pouya Sadri on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CompossAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
